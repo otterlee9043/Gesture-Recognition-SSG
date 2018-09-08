e15580a5778b6265a15b430311fda7567aaa4191
otterlee <21700525@handong.edu>
2018-09-08 17:49:11 +0900

    주석 추가

--- normalization/data normalization.py
+++ normalization/data normalization.py
@@ -13,15 +13,15 @@ from scipy import stats
 import tensorflow as tf
 import math
 import random
-###################################################
+###################################################sua
 
 
 # In[10]:
 
 
-fps=40
-threshold=0.75
-moving_avg_len=10
+fps=120 #데이터 자를 때 사용될 값, 1초 당 frame 수 
+threshold=0.75 #구간 나눌 때 데이터 변화를 구분하기 위한 수
+moving_avg_len=10 #smoothing할 때
 
 
 # In[17]:
@@ -35,20 +35,6 @@ def readFileData(file):
     wx = data["wmx1"]
     wy = data["wmy1"]
     wz = data["wmz1"]
-#     bx1 = data["bmx1"] 
-#     by1 = data["bmy1"]
-#     bz1 = data["bmz1"]
-#     bx2 = data["bmx2"]
-#     by2 = data["bmy2"]
-#     bz2 = data["bmz2"]
-#     bx3 = data["bmx3"]
-#     by3 = data["bmy3"]
-#     bz3 = data["bmz3"]
-
-#     w=np.dstack([wx,wy,wz])
-#     b1=np.dstack([bx1,by1,bz1])
-#     b2=np.dstack([bx2,by2,bz2])
-#     b3=np.dstack([bx3,by3,bz3])
     
     records=movingavg(wx,wy,wz)
 
@@ -61,14 +47,14 @@ def readData(directory):
     allFiles = glob.glob("*.csv")
     for i,file in enumerate(allFiles):
         fileName = os.path.basename(file)
-        print("file name is", fileName,"num is",i)
-        (name, ext) = os.path.splitext(fileName)
-        parts = name.split("_")
+        #print("file name is", fileName,"num is",i)
+        (name, ext) = os.path.splitext(fileName) #파일 이름 확장자나누기
+        parts = name.split("_") #제스처 종류
         if (True):
             label = parts[0]
-            fileData = readFileData(file)
+            fileData = readFileData(file) #값을 읽어서 묶음
             print("file datat is ",fileData)
-            records.append(fileData)
+            records.append(fileData) #record array에 추가
             labels = np.append(labels, label)
 
     return (records, labels)
@@ -85,18 +71,18 @@ def movingavg(x,y,z):
 
 def splitData(records):
     print("records  is ",records[0])
-    pd_records=pd.DataFrame(records[0])
+    pd_records=pd.DataFrame(records[0]) #null 값 0으로 처리
     record_w=pd_records.fillna(0)
-    term=fps//4
-    record_sum=[]
-    records=[]
-    ct=0
-    sum_now=0.0
-    sum_pre=0.0
+    term=fps//4  #프레임 30단위로 변화량 측정
+    record_sum=[] 
+    records=[] #구간 단위로 나뉘어진 data 담는 array
+    ct=0 #구간 몇갠지 count
+    sum_now=0.0 #변화량1
+    sum_pre=0.0 #변화량
     
-    for i in range((len(records[0])//(term))-20):
+    for i in range((len(records[0])//(term))-20): #
         sum=0.0
-        record_x=np.empty(shape=[1],dtype=float)
+        record_x=np.empty(shape=[1],dtype=float) #array 선언
         record_y=np.empty(shape=[1],dtype=float)
         record_z=np.empty(shape=[1],dtype=float)
         for j in range(fps):
@@ -110,7 +96,6 @@ def splitData(records):
         sum_now=sum
         record_sum.append(sum)
         if (sum_pre<threshold and sum_now>threshold):
-            
             ct=ct+1
             start=120*i
         if (sum_pre>threshold and sum_now<threshold):
